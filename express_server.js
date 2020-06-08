f1dbedf345e7fcb3c3264e0d333bb43e2c26aa1b
Sophie <sophiemargueritedubois@gmail.com>
2020-06-08 22:13:11 +0000

    Added get request for the '/u/:shortURL' which will redirect the user to the long URL

--- express_server.js
+++ express_server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let str = '';
+  while (str.length !== 6) {
+    str += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return str;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,15 +36,26 @@ app.get('/urls/new', (req, res) => {
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  // console.log(req.params.shortURL);
+  res.redirect(urlDatabase[req.params.shortURL]);
 })
 
 app.get('/urls.json', (req, res) => {
   res.send(urlDatabase);
 });
 
-
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n')
+});
+
+app.post('/urls', (req, res) => {
+  let randomString = generateRandomString();
+  urlDatabase[randomString] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${randomString}`);
 });
 
 app.listen(PORT, () => {
