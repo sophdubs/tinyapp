98149c14cb8c36891caf6f970be2aafde9f9e71f
Sophie <sophiemargueritedubois@gmail.com>
2020-06-08 21:47:46 +0000

    Added path and view for urls/new so us users can see a form to submit a new url

--- express_server.js
+++ express_server.js
@@ -18,6 +18,10 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
@@ -26,6 +30,7 @@ app.get('/urls/:shortURL', (req, res) => {
 app.get('/urls.json', (req, res) => {
   res.send(urlDatabase);
 });
+
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n')
