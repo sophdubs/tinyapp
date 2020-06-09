c08b01bd5d940afc17421af56717abd66d76586a
Sophie <sophiemargueritedubois@gmail.com>
2020-06-09 20:30:45 +0000

    Added conditional forms in header.ejs. . It prompts the user to login if no cookies are found, or displays the user name and a logout button if a username cookie is found

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 function generateRandomString() {
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -25,16 +27,26 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies['username']
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies['username']
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies['username']
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -69,7 +81,9 @@ app.post('/urls/:shortURL', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  res.send('ok');
+  console.log(req.body);
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
