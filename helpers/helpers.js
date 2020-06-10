44b4f05dfd3cd092427040e29ae9339556cef565
Sophie <sophiemargueritedubois@gmail.com>
2020-06-10 21:41:13 +0000

    Added comments to helpers.js and fixed all eslint errors for this file

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,5 +1,6 @@
 const { users } = require('../models/user_data');
 
+// Adds user to db
 const addUserToDB = function(email, password, userID) {
   users[userID] = {
     id: userID,
@@ -8,7 +9,8 @@ const addUserToDB = function(email, password, userID) {
   };
 };
 
-function generateRandomString() {
+// Generates a random string of 6 characters using A-Z, a-z, 0-9 chars.
+const generateRandomString = function() {
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let str = '';
   while (str.length !== 6) {
@@ -17,10 +19,12 @@ function generateRandomString() {
   return str;
 };
 
+// Returns boolean based on if user email is already in user database.
 const userExists = email => {
   return (Object.values(users).find(user => user.email === email) !== undefined);
 };
 
+// Returns user matching the given email
 const findUserByEmail = email => {
   const user = Object.values(users).find(user => user.email === email);
   return user;
