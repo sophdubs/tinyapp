9b70874f612c9968663a829e793448b8f1167b7c
Sophie <sophiemargueritedubois@gmail.com>
2020-06-11 17:49:19 +0000

    added test for generateRandomString

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -21,7 +21,19 @@ describe('#addUserToDB', function() {
     const email = 'test@gmail.com';
     const password = "password";
     addUserToDB(email, password, id, testUsers);
+    // Checking that the entry exists
     expect(testUsers['1234']).to.not.be.undefined;
+    // Checking that the user object matches the expected user object
     expect(testUsers['1234']).to.deep.equal({id: '1234', email: 'test@gmail.com', password: 'password'});
+  });
+});
+
+describe('#generateRandomString', function() {
+  it('should generate a random alphanumeric string of 6 characters', function() {
+    let randomString = generateRandomString();
+    // Checking that string is alphanumeric
+    expect(!'randomString'.match(/^[A-Za-z0-9]+$/)).to.be.false;
+    // Checking that string is of length 6
+    expect(randomString.length).to.equal(6);
   });
 });
